66281d1d20dd8b680b806410795466cad7937116
prakash123-debug <yatripr987@gmail.com>
2022-01-25 12:00:39 +0545

    work-on-completion

--- src/components/Tables/CategoryAppTable/CategoryTable/CategoryTable.js
+++ src/components/Tables/CategoryAppTable/CategoryTable/CategoryTable.js
@@ -1,44 +1,75 @@
-
-
-import React, { useEffect,useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Row, Table } from "react-bootstrap";
-
+import { baseLink } from "../../../Helpers/Axios";
+import { ListViewComponent } from '@syncfusion/ej2-react-lists';
 import Node from './Node';
+import '../CategoryStyle.css'
 import axiosInstance from "../../../Helpers/Axios";
-const CategoryTable = ({show}) => {
+const CategoryTable = ({ show }) => {
+  const [items, SetItems] = useState([]);
+  useEffect(() => {
+    axiosInstance
+      .get("/category", {
+        headers: {
+          "Content-Type": "application/json"
 
-    const [items,SetItems] = useState([]);
-    useEffect(() => {
-      axiosInstance
-        .get("/category", {
-          headers: {
-            "Content-Type": "application/json"
-          
-          },
-        })
-        .then((res) => {
-          SetItems(res.data.data);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    },[show]);
+        },
+      })
+      .then((res) => {
+        console.log("data shown is",res.data.data);
+        SetItems(res.data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [show]);
+  const fields = {
+    id:"id",text:"name"
+  }
 
+  function listTemplate(data) {
+             return( <div className="card h-100 shadow-sm">
+                <div className="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                  <img  src={`${baseLink}${data.image}`} className="img-fluid" />
+                  <a href="#!">
+                    <div className="mask" style={{ backgroundColor: "rgba(251, 251, 251, 0.15)" }}></div>
+                  </a>
+                </div>
+                <div className="card-body">
+                  <h5 className="card-title">{data.name}</h5>
+                  <span>sub category:</span>
+                 {
+                   data.child.length !==0? 
+                     (
+                      data.child.map((childData,index)=>{
+                        return(
+                         <span className="card-text">{childData.name} {' '}</span>
+                        )
+                      })
+                     ):<span>null</span>
+                 }
+                  <button disabled={data.child.length ==0 ? true : false} className="btn btn-primary">Explore more category</button>
+                </div>
+              </div>)
 
-    let nodes = items.map(function(data) {
-      return (
-        <Node node={data} children={data.children} />
-      );
-    });
+    // return (<div className='container'>
+    //   {letterAvatar}
+    //     <span className="e-list-item-header">{data.name}</span>
+    // </div>);
+}
 
-    return (
-    
-          <div className="BreadcrumbStyle mt-4">
-      <Table className="table table-bordered w-100">
-             {nodes}
-      </Table>
-      </div>
-    );
+  // let nodes = items.map(function (data) {
+  //   return (
+  //     <Node node={data} children={data.children} />
+  //   );
+  // });
+
+  return (
+    <div className="BreadcrumbStyle BreadcrumbTitle row mt-3">
+      <ListViewComponent id="div" className="lists-style" dataSource={items} template={listTemplate}  fields={fields} showHeader={true} headerTitle="Categories"/>
+   </div>
+  )
+   
 }
 
 export default CategoryTable;
@@ -48,3 +79,52 @@ export default CategoryTable;
       //       {nodes}
       //   </Row>
       // </div>
+       // <div className="container mt-5">
+    //   <div className="row">
+    //     {console.log("nodes",nodes)}
+    //     {nodes}
+      
+        // {/* {
+        //   items.map((data, index) => {
+        //     return(
+        //       <div className="col-md-4">
+        //       <div className="card">
+        //         <div className="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+        //           <img  src={`${baseLink}${data.image}`} className="img-fluid" />
+        //           <a href="#!">
+        //             <div className="mask" style={{ backgroundColor: "rgba(251, 251, 251, 0.15)" }}></div>
+        //           </a>
+        //         </div>
+        //         <div className="card-body">
+        //           <h5 className="card-title">{data.name}</h5>
+        //           <span>sub category:</span>
+        //          {
+        //            data.children.length !==0? 
+        //              (
+        //               data.children.map((childData,index)=>{
+        //                 return(
+        //                  <span className="card-text">{childData.name} {' '}</span>
+        //                 )
+   
+        //               })
+
+        //              ):<span>null</span>
+                 
+        //          }
+        //           <a href="#!" className="btn btn-primary">Explore more Categories</a>
+        //         </div>
+        //       </div>
+        //     </div>
+        //     )
+        //   })
+        // }
+        
+    //   </div>
+    // </div>
+
+    //     <div className="BreadcrumbStyle mt-4">
+    // <Table className="table table-bordered w-100">
+    //        {nodes}
+    // </Table>
+    // </div>
+  // );
--- src/components/Tables/CategoryAppTable/CategoryTable/Node.js
+++ src/components/Tables/CategoryAppTable/CategoryTable/Node.js
@@ -1,7 +1,9 @@
 
 import React from 'react';
 import { baseLink } from "../../../Helpers/Axios";
+
 function Node({node,children}) {
+  
   let childnodes = null;
 
   if(children) {
@@ -12,27 +14,47 @@ function Node({node,children}) {
    });
   }
 
+  const handleClick = (nodeDatas)=>{
+    console.log(nodeDatas)
+
+  }
+
   return (
     <>
-    <tr className="bg-primary">
-      <td>category</td>
-      <td>image</td>
-     
-    </tr>
-    <tr  className="bg-success">
-      <td><span>{node.name}</span>
-      { childnodes ?
-        <ul >{childnodes}</ul>
-      : null }
-      </td>
-      <td>
-      <img
-        className="imagePreviewTable ml-5"
-        src={`${baseLink}${node.image}`}
-        alt="imagepreview"
-     />
-      </td>
-    </tr>
+     <div className="col-md-4">
+              <div className="card">
+                <div className="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                  <img  src={`${baseLink}${node.image}`} className="img-fluid" />
+                  <a href="#!">
+                    <div className="mask" style={{ backgroundColor: "rgba(251, 251, 251, 0.15)" }}></div>
+                  </a>
+                </div>
+                <div className="card-body">
+                  <h5 className="card-title">{node.name}</h5>
+                  <span>sub category:</span>
+                  {
+                   node.children.length !==0? 
+                     (
+                      node.children.map((childData,index)=>{
+                        return(
+                         <span className="card-text">{childData.name} {' '}</span>
+                        )
+   
+                      })
+
+                     ):<span>null</span>
+                 
+                 }
+
+                  {/* { childnodes ?
+                    <div>{childnodes}</div>
+                  : null } */}
+                  <a href="#!" className="btn btn-primary" onClick={handleClick({childnodes})}>Explore more Categories</a>
+                </div>
+              </div>
+            </div>
+            {childnodes}
+ 
 
     </>
 
@@ -50,7 +72,29 @@ export default Node;
     //  />
      
     //   <hr/>
+      // { childnodes ?
+      //   <ul >{childnodes}</ul>
+      // : null }
+    // </div>
+
+
+    //   <tr className="bg-primary">
+    //   <td>category</td>
+    //   <td>image</td>
+     
+    // </tr>
+    // <tr  className="bg-success">
+    //   <td><span>{node.name}</span>
     //   { childnodes ?
     //     <ul >{childnodes}</ul>
     //   : null }
-    // </div>
+    //   </td>
+    //   <td>
+    //   <img
+    //     className="imagePreviewTable ml-5"
+    //     src={`${baseLink}${node.image}`}
+    //     alt="imagepreview"
+    //  />
+    //   </td>
+    // </tr> 
+
